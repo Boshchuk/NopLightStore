c28ff2f3040e0416474202cdf649db51176b36c3
Victor_Home <nakedsnake@inbox.ru>
2015-05-07 18:04:32 +0300

    Project updated

---
+++ Libraries/Nop.Ncc/NccImportManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Services.ExportImport;
+
+namespace Nop.Ncc
+{
+    public class NccImportManager : IImportManager
+    {
+        /// <summary>
+        /// Import products from XLSX file
+        /// </summary>
+        /// <param name="stream">Stream</param>
+        public void ImportProductsFromXlsx(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ImportNewsletterSubscribersFromTxt(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ImportStatesFromTxt(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
